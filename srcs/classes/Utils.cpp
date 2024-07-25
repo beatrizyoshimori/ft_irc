c274dbc54feb3fa185d28dee5b16c2662c7bf036
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-07-24 21:18:17 -0300

    added header

--- srcs/classes/Utils.cpp
+++ srcs/classes/Utils.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 21:16:01 by byoshimo          #+#    #+#             */
+/*   Updated: 2024/07/24 21:17:50 by byoshimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Utils.hpp"
 
 std::vector<std::string> Utils::split(std::string str, std::string delimiter)
@@ -12,8 +24,10 @@ std::vector<std::string> Utils::split(std::string str, std::string delimiter)
 	{
 		tokens.push_back(str.substr(start, end - start));
 		start = end + del_size;
+		// std::cout << "tokens.back: ." << tokens.back() << "." << std::endl;
 	}
 	tokens.push_back(str.substr(start));
 	tokens.erase(std::remove(tokens.begin(), tokens.end(), ""), tokens.end());
+	// std::cout << "tokens.back2: ." << tokens.back() << "." << std::endl;
 	return (tokens);
 }
--- srcs/commands/NICK.cpp
+++ srcs/commands/NICK.cpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Utils.hpp                                          :+:      :+:    :+:   */
+/*   NICK.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/24 21:16:32 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/07/24 21:16:34 by byoshimo         ###   ########.fr       */
+/*   Created: 2024/07/24 21:15:53 by byoshimo          #+#    #+#             */
+/*   Updated: 2024/07/24 21:15:54 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_HPP
-# define UTILS_HPP
-
-#include "ft_irc.hpp"
-
-class	Utils
-{
-	public:
-		Utils(void);
-		~Utils(void);
-
-		static std::vector<std::string>	split(std::string str, std::string delimiter);
-};
-
-#endif
