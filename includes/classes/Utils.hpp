60f3764c6231e3162df9d24b0ba02eaf134ff6df
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-07-24 21:33:41 -0300

    updated class

--- includes/classes/Utils.hpp
+++ includes/classes/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:16:32 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/07/24 21:16:34 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/07/24 21:32:42 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 class	Utils
 {
 	public:
-		Utils(void);
-		~Utils(void);
+		Utils(void) {};
+		~Utils(void) {};
 
 		static std::vector<std::string>	split(std::string str, std::string delimiter);
 };
