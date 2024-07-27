b9a7fc852b8899ded046a93cf82f4637f2a67765
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-07-27 19:19:57 -0300

    added replies 332, 353, 366 and errors 471, 473, 475

--- includes/replies.hpp
+++ includes/replies.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:16:24 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/07/24 21:16:26 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/07/27 18:47:34 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # define CRLF std::string("\r\n")
 
 # define RPL_WELCOME(nick, user)					(FTIRC + " 001 " + user + " :Welcome to the Internet Relay Chat " + nick + "!" + user + "@*" + CRLF)
+# define RPL_TOPIC(nick, channel, topic)			(FTIRC + " 332 " + nick + " " + channel + " :" + topic + CRLF)
+# define RPL_NAMREPLY(nick, channel, names)			(FTIRC + " 353 " + nick + " = " + channel + " :" + names + CRLF)
+# define RPL_ENDOFNAMES(nick, channel)				(FTIRC + " 366 " + nick + " " + channel + " :End of NAMES list" + CRLF)
 
 # define ERR_NOSUCHCHANNEL(channel)					(FTIRC + " 403 * " + channel + ":No such channel")
 # define ERR_NONICKNAMEGIVEN()						(FTIRC + " 431 :No nickname given" + CRLF)
@@ -25,5 +28,10 @@
 # define ERR_NEEDMOREPARAMS(command, reason)		(FTIRC + " 461 * " + command + " :" + reason + CRLF)
 # define ERR_ALREADYREGISTRED(user)					(FTIRC + " 462 * " + user + ":User already registered)" + CRLF)
 # define ERR_PASSWDMISMATCH()						(FTIRC + " 464 * :Password incorrect" + CRLF)
+# define ERR_CHANNELISFULL(channel)					(FTIRC + " 471 * " + channel + ":Cannot join channel (Channel is full)" + CRLF)
+# define ERR_INVITEONLYCHAN(channel)				(FTIRC + " 473 * " + channel + ":Cannot join channel (Invite only channel)" + CRLF)
+# define ERR_BADCHANNELKEY(channel)					(FTIRC + " 475 * " + channel + ":Cannot join channel (Incorrect password)" + CRLF)
+
+# define JOIN(user, channel)						(":" + user + " JOIN " + channel + CRLF)
 
 #endif
