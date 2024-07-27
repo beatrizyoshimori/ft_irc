a7890249dfc8dfc15cd1ca5e69fad1a0907d6b26
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-07-27 19:18:07 -0300

    added addChannel function

--- includes/classes/Server.hpp
+++ includes/classes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 19:10:30 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/07/20 20:19:42 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/07/27 18:18:47 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ class	Server
 		std::string				getServerPassword(void) const;
 		std::vector<Client>		getClients(void) const;
 		std::vector<Channel>	getChannels(void) const;
+		void					addChannel(Channel &channel);
 };
 
 #endif
