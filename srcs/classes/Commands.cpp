5051c119c136d240072a496a1de1513513417f38
Beatriz Yoshimori <beatrizyoshimori@MacBook-Air-de-Beatriz.local>
2024-07-30 23:28:54 -0300

    command PART done

--- srcs/classes/Commands.cpp
+++ srcs/classes/Commands.cpp
@@ -25,17 +25,20 @@ void	Commands::populateMap(void)
 	// _commands["LISTC"] = listc;
 	// _commands["PRIVMSG"] = privmsg;
 	// _commands["KICK"] = kick;
-	// _commands["PART"] = part;
+	_commands["PART"] = part;
 	// _commands["WHO"] = who;
 	// _commands["MODE"] = mode;
 	// _commands["TOPIC"] = topic;
 	// _commands["INVITE"] = invite;
 }
 
-std::string	Commands::callFunction(CommandArgs &cArgs)
+void	Commands::callFunction(CommandArgs &cArgs)
 {
 	//Check user authentication
 	if (_commands.find(cArgs.msg.command) == _commands.end())
-		return (cArgs.msg.command + " :Invalid command\r\n");
-	return (_commands[cArgs.msg.command](cArgs));
+	{
+		std::cerr << "Invalid command\r\n" << std::endl;
+		return ;
+	}
+	_commands[cArgs.msg.command](cArgs);
 }
