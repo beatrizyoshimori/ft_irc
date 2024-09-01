50932dc665206069c718a246f6ad8e6804342e25
beatrizyoshimori <beatrizyoshimori@gmail.com>
2024-09-01 15:34:30 -0300

    updated reply 324

--- includes/replies.hpp
+++ includes/replies.hpp
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:16:24 by byoshimo          #+#    #+#             */
-/*   Updated: 2024/08/31 17:45:57 by byoshimo         ###   ########.fr       */
+/*   Updated: 2024/09/01 15:17:48 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # define CRLF std::string("\r\n")
 
 # define RPL_WELCOME(nick, user)						(FTIRC + " 001 " + user + " :Welcome to the Internet Relay Chat " + nick + "!" + user + "@*" + CRLF)
-# define RPL_CHANNELMODEIS(nick, channel, mode)			(FTIRC + " 324 " + nick + " " + channel + " " + mode + CRLF)
+# define RPL_CHANNELMODEIS(nick, channel, mode, params)	(FTIRC + " 324 " + nick + " " + channel + " " + mode + params + CRLF)
 # define RPL_NOTOPIC(nick, channel)						(FTIRC + " 331 " + nick + " " + channel + " :No topic is set" + CRLF)
 # define RPL_TOPIC(nick, channel, topic)				(FTIRC + " 332 " + nick + " " + channel + " :" + topic + CRLF)
 # define RPL_INVITING(nick, recipient, channel)			(FTIRC + " 341 " + nick + " " + recipient + " :" + channel + CRLF)
